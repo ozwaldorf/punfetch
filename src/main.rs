45626ad1d040812a9bb04b2662474940641d1e86
ozwaldorf <self@ossian.dev>
2023-01-08 01:42:16 -0500

    feat: default ascii logo, --show-logo feature parity

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,49 @@
+use std::fmt::format;
 use clap::{arg, command};
 use image::open;
-use info::*;
 use onefetch_image::get_best_backend;
 use sysinfo::{System, SystemExt};
+use term_size::dimensions;
+
+use info::*;
 
 mod info;
 
+const LINUX: &str = r#"               _nnnn_
+              dGGGGMMb
+             @p~qp~~qMb
+             M|@||@) M|
+             @,----.JM|
+            JS^\__/  qKL
+           dZP        qKRb
+          dZP          qKKb
+         fZP            SMMb
+         HZM            MMMM
+         FqM            MMMM
+       __| ".        |\dS"qML
+       |    `.       | `' \Zq
+      _)      \.___.,|     .'
+      \____   )MMMMMP|   .'
+           `-'       `--'
+ "#;
+
 fn main() {
     let args = command!()
-        .arg(arg!(-i --image <PATH> "Image to use"))
+        .args(vec![
+            arg!(-i --image <PATH> "Image to use"),
+            arg!(--"show-logo" <WHEN> "Show logo (always, never, auto). If set to auto: the logo will be hidden if the terminal's width < 95"),
+        ])
         .about("")
         .get_matches();
+
+    let show_logo = match args.get_one::<String>("show-logo").unwrap_or(&"always".to_string()).as_str() {
+        "always" => true,
+        "auto" => {
+            dimensions().unwrap_or((0, 0)).0 >= 95
+        }
+        "never" => false,
+        _ => panic!("Invalid value for --show-logo. Valid values are: always, never, auto"),
+    };
 
     let mut sys = System::new_all();
     sys.refresh_all();
@@ -22,15 +55,25 @@ fn main() {
 
     let mut buf = String::new();
 
-    if let Some((image, backend)) = args.get_one("image").map(|path: &String| {
-        let image = open(path).expect("Failed to open image");
-        let backend = get_best_backend().expect("Failed to find a backend");
-        (image, backend)
-    }) {
-        buf.push_str(backend.add_image(info, &image, 32).unwrap().as_str());
+    if show_logo {
+        if let Some((image, backend)) = args.get_one("image").map(|path: &String| {
+            let image = open(path).expect("Failed to open image");
+            let backend = get_best_backend().expect("Failed to find a backend");
+            (image, backend)
+        }) {
+            buf.push_str(backend.add_image(info, &image, 32).unwrap().as_str());
+        } else {
+            let art: Vec<&str> = LINUX.lines().collect();
+            let art_width = (art.iter().map(|s| s.len()).max().unwrap_or(0) + 6).max(art.len());
+            for i in 0..art.len().max(info.len()) {
+                let line = info.get(i).map(|i| i.to_string()).unwrap_or_default();
+                let art = art.get(i).map(|i| i.to_string()).unwrap_or_default();
+                buf.push_str(&format!("{:<art_width$}{}\n", art, line));
+            }
+        }
     } else {
         for line in info {
-            buf.push_str(format!("{line}\n").as_str());
+            buf.push_str(&format!("{}\n", line));
         }
     }
 
